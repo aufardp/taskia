b429228739931b16aaf28a54b1e8cb7fee40821a
aufardp <aufardanu@gmail.com>
2025-08-04 17:07:15 +0700

    feat: display data task and create format date

--- js/addTask.js
+++ js/addTask.js
@@ -1,6 +1,12 @@
 // File ini untuk penghubung antara UI HTML dan model User
 
 document.addEventListener("DOMContentLoaded", () => {
+  //membuat tanggal yang diharapkan berformat 'yyyy-mm-dd'
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+
   const taskForm = document.getElementById("taskForm");
   const taskManager = new Task();
 
@@ -10,6 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskData = {
       taskName: document.getElementById("taskName").value,
       taskPriority: document.getElementById("taskPriority").value,
+      createdAt: `${year}-${month}-${day}`,
     };
 
     const result = taskManager.saveTasks(taskData);
